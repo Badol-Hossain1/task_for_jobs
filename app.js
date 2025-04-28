29540ac2d0705f960f4a83bc7264d630b0e02162
Badol-Hossain1 <badolhossain586@gmail.com>
2025-04-28 13:27:44 +0600

    navbar

---
+++ app.js
@@ -0,0 +1,18 @@
+
+
+let menuList = document.getElementById("menuList")
+menuList.style.maxHeight = "0px"
+
+function toggleMenu(){
+    if(menuList.style.maxHeight == "0px"){
+   menuList.style.background = "white"
+       menuList.style.maxHeight = "254px"
+      
+      
+    }
+    else{
+        menuList.style.maxHeight = "0px"
+          menuList.style.background = ""
+       
+    }
+}
